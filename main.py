841a7dfacdc1fa5debd5e12afcf346ef09892d48
QazRT <76099153+QazRT@users.noreply.github.com>
2025-02-26 16:44:35 +0300

    Created

---
+++ main.py
@@ -0,0 +1,12 @@
+﻿from fastapi import FastAPI
+from random import randint
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {'massage': str(randint(1, 1000))}    
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8080)
