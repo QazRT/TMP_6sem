311c6d25ab9199692533400587e064845d0554f7
QazRT <76099153+QazRT@users.noreply.github.com>
2025-02-26 17:30:18 +0300

    Upd

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-﻿from fastapi import FastAPI
-from random import randint
+from fastapi import FastAPI
+import hashlib as hsl
+import base64 as b64
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {'massage': str(randint(1, 1000))}    
+class User(BaseModel):
+    username: str
+    password: str
+
+@app.post("/user/string")
+async def root(user: User):
+    passwordHash = hsl.sha256(f"{user.username}+{user.password}".encode(), usedforsecurity=True).hexdigest()
+    return {'userstring': b64.encodebytes(str(user.username+':'+passwordHash).encode()).strip()}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8080)
+    uvicorn.run(app, host='127.0.0.1', port=8080)
