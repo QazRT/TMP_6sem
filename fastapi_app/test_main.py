87d24bcf31c7c9e0f0ee571c998ce9f7316128b9
QazRT <76099153+QazRT@users.noreply.github.com>
2025-04-09 17:54:16 +0300

    fix

--- fastapi_app/test_main.py
+++ fastapi_app/test_main.py
@@ -3,11 +3,13 @@ from main import app
 
 client = TestClient(app)
 
+uniq = __import__("random").randint(0, 100000)
+
 def test_create_user():
-    response = client.post("/users/", json={"name": "John Doe", "email": "john@example.com"})
+    response = client.post("/users/", json={"name": "John Doe", "email": f"john{uniq}@example.com"})
     assert response.status_code == 200
     assert response.json()["name"] == "John Doe"
-    assert response.json()["email"] == "john@example.com"
+    assert response.json()["email"] == f"john{uniq}@example.com"
 
 def test_read_users():
     response = client.get("/users/")
